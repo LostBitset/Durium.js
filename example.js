ebb0cd0fecfa85217d57887f6cf95e6470747191
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-10 18:16:37 -0400

    try to clear input

--- example.js
+++ example.js
@@ -10,10 +10,20 @@ function todo_list() {
 			du("li", du.value())
 		))
 	);
+	// Create an <input> element that will be the source for new_todos
+	const input = du("input", { value: new_todos.source });
+	// When new_todos fires, clear the value attribute of the <input>
+	new_todos.subscribe(() => {
+		// You should be careful using getElement, actual HTML element
+		// doesn't exist until the component is mounted
+		// Just make sure it won't be executed immediately
+		// In this case we're totally fine
+		input.getElement().value = "";
+	});
 	// Setup the station, and have the layout display it
 	// du.div(...) is just a shorthand for du("div", ...)
 	return du.div(
-		du("input", { value: new_todos.source }),
+		input,
 		du("button", { onclick: new_todos.fire }, "Add!"),
 		list.of(todos)
 	);
