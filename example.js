2b83ae367c66a9bd1c45fc43ccbbddd853c80ca3
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-10 16:14:00 -0400

    components are functions!

--- example.js
+++ example.js
@@ -1,24 +1,31 @@
-// Create a station that sends out new todos
-const new_todos = new Station();
-// Create a new station that sends out the current list of todos
-const todos = new_todos.history();
-// Create a layout the displays a list as an HTML <ul>
-const list = du.layout(
-  du("ul", du.repeat(
-    du("li", du.value())
-  ))
-);
-// Create a component using that layout and station
-const todo_list = du.component(
-  du("input", { value: todos.source }),
-  du("button", { onclick: todos.fire }, "Add!"),
-  list.of(todos)
-);
+// Components are just functions
+function todo_list() {
+	// Create a station that sends out new todos
+	const new_todos = new Station();
+	// Create a new station that sends out the current list of todos
+	const todos = new_todos.history();
+	// Create a layout the displays a list as an HTML <ul>
+	const list = du.layout(
+	  du("ul", du.repeat(
+	    du("li", du.value())
+	  ))
+	);
+	// Setup the station, and have the layout display it
+	// du.template(...) is just shorthand for du("template", ...)
+	return = du.template(
+	  du("input", { value: todos.source }),
+	  du("button", { onclick: todos.fire }, "Add!"),
+	  list.of(todos)
+	);
+}
 // Create a component that uses the `todo_list` component
-const app = du.component(
-  du("h1", "Example App"),
-  todo_list.use()
-);
+// Remember, they're just functions
+function app() {
+	return du.template(
+		du("h1", "Example App"),
+		todo_list()
+	);
+}
 // Mount this new component
-app.mount(document.body);
+du.mount(app, document.body);
 
